3312306eb7ef50d566e153af6c9542563a0fedd7
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2022-12-13 15:57:56 +0300

    Update scripts files Update index.html Update index.css

---
+++ modules/Classes.js
@@ -0,0 +1,82 @@
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-use-before-define */
+
+// Creat Book class
+class Book {
+  constructor(title, author) {
+    this.bookid = Math.random().toFixed(1);
+    this.title = title;
+    this.author = author;
+  }
+}
+
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  // Add a new book
+  addBook(newItem) {
+    this.books.push(newItem);
+    localStorage.setItem('DB', JSON.stringify(this.books));
+    Display(newItem);
+  }
+
+  // remove a book
+  remove(bookid) {
+    const rm = document.getElementById(bookid);
+    rm.remove();
+    this.books = this.books.filter((x) => x.bookid !== bookid);
+    localStorage.setItem('DB', JSON.stringify(this.books));
+  }
+}
+const storeBook = new Library();
+
+// fetch values from input
+const getInput = () => {
+  const title = document.querySelector('#book-title').value;
+  const author = document.querySelector('#book-author').value;
+  const addItem = new Book(title, author);
+  return addItem;
+};
+// Showcase the list of books
+let Display = (index) => {
+  const bookList = document.querySelector('.library');
+  const items = document.createElement('tr');
+  items.setAttribute('id', index.bookid);
+  items.classList.add('items');
+  items.innerHTML = `<th> "${index.title}" by ${index.author}</th>`;
+  const rmbtn = document.createElement('button');
+  rmbtn.innerHTML = 'Remove';
+  rmbtn.setAttribute('id', 'removebtn');
+  rmbtn.addEventListener('click', () => storeBook.remove(index.bookid));
+  items.appendChild(rmbtn);
+  bookList.appendChild(items);
+};
+const error = () => {
+  const span = document.querySelector('.alert');
+  span.textContent = 'Fields required';
+  span.style.color = 'red';
+  setTimeout(() => span.remove(), 3000);
+};
+// Add button
+const addNewBook = document.getElementById('addbtn');
+const title = document.querySelector('#book-title');
+const author = document.querySelector('#book-author');
+addNewBook.addEventListener('click', (event) => {
+  if (title.value == '' || author.value == '') {
+    event.preventDefault();
+    error();
+  } else {
+    const book = getInput();
+    storeBook.addBook(book);
+  }
+});
+window.onload = () => {
+  storeBook.books = JSON.parse(localStorage.getItem('DB' || '[]'));
+  if (storeBook.books === null) {
+    storeBook.books = [];
+    return;
+  }
+  storeBook.books.forEach((item) => Display(item));
+};
---
+++ modules/dateTime.js
@@ -0,0 +1,12 @@
+import { DateTime } from './luxon.js';
+
+const today = document.getElementById('date');
+const setTime = () => {
+  const now = DateTime.now();
+  const date = now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+  today.textContent = date;
+};
+
+setInterval(setTime, 1000);
+
+// eslint-disable-next-line import/prefer-default-export
